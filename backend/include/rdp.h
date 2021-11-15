4ae21b68a8afd61e759a8233715c392f6bb027e0
wawadish <wawadish@gmail.com>
2021-11-15 08:20:27 -0500

    Had a perfect solution for 1-1 connection, tried to adapt to 1-many, messed up my pointers

--- backend/include/rdp.h
+++ backend/include/rdp.h
@@ -3,6 +3,8 @@
 
 #define RDP_FLAG 32768 //last bit
 #define ACK_FLAG 16384 //second to last bit
+#define SEQ_BIT_1 8192 //second to last bit
+#define SEQ_BIT_0 4096 //second to last bit
 
 // Comment the line below to not see any debug prints
 #define RDP_DEBUG
@@ -14,28 +16,41 @@
 #endif
 
 
-// Is called during gnc while loop when waiting for user input
+/*
+ * RDP packets sent from user
+ */
 err_t
 rdp_send(struct udp_pcb *pcb, struct pbuf *p);
 
-// Calls udp_send with RDP_FLAG turned on
-err_t
-rdp_sendto(struct udp_pcb *pcb, struct pbuf *p, uchar *dst_ip, uint16_t dst_port);
+struct rdp_connection_node* rdp_create_connection(struct rdp_connection_node* connection, uchar *addr, uint16_t port);
 
-// Uses a timeout mechanism to repetitively call rdp_send_to upon timeout
-err_t
-rdp_timed_send(struct udp_pcb *pcb, struct pbuf *p);
+int* retrieve_local_state(struct udp_pcb *pcb, uchar *addr, uint16_t port);
 
-// Sends a packat with the ACK_FLAG turned on
+void handle_in_seq_recv(struct udp_pcb *pcb, struct pbuf *p, int seq, uchar *addr, uint16_t port, int* state);
+
+void handle_out_of_seq_recv(struct udp_pcb *pcb, int seq, uchar *addr, uint16_t port);
+
+void rdp_recv_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, uchar *addr, uint16_t port, bool is_rdp, bool is_ack, int seq);
+
+bool rdp_same_state(int state, int seq);
+
+void rdp_recv_ack(struct udp_pcb *pcb, int* state, int seq);
+
 err_t
-rdp_send_ack(struct udp_pcb *pcb, uchar *addr, uint16_t port);
+rdp_timed_send(struct udp_pcb *pcb, struct pbuf *p, int* state);
+
+err_t
+rdp_sendto(struct udp_pcb *pcb, struct pbuf *p, uchar *dst_ip, uint16_t dst_port, int* state);
+
+int get_state_flag(int* state);
+
+void next_state(int* state);
+
+err_t
+rdp_send_ack(struct udp_pcb *pcb, int seq, uchar *addr, uint16_t port, int* state);
+
 struct pbuf* create_ack_pbuf();
 
-// Is called by udp_recv_callback whenever the RDP_FLAG is set
-void rdp_recv_callback(void *arg, struct udp_pcb *pcb, struct pbuf *p, uchar *addr, uint16_t port, bool is_rdp, bool is_ack);
-
-/*
-    DEBUG METHODS
-*/
 void printPCB(struct udp_pcb *pcb);
+
 void print_ip_and_port(uchar *addr, uint16_t port);
